28a54696cd2ce4ada6bf9575b42c31be6fa754df
rozetadavchevska <rozetadavcevska@gmail.com>
2022-12-08 20:56:07 +0100

    Simple API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=8de1536';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const searchMovies = async(title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  },[]);
+
   return (
     <div className="App">
-      
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input 
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+        <FontAwesomeIcon icon={faMagnifyingGlass} size='xl' onClick={()=>searchMovies(searchTerm)}/>
+      </div>
+
+      {
+        movies?.length>0
+        ?(
+          <div className='container'>
+            {
+              movies.map((movie)=>(
+                <MovieCard movie={movie}/>
+              ))
+            }
+          </div>
+        ) : (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )      
+      }
+
     </div>
   );
 }
